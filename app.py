ddaa855c5fd1a0f5bc56e375b4671a21afaabaa9
Will N <146034825+HelpItsAllGooberedUp@users.noreply.github.com>
2025-12-29 20:36:49 -0600

    initial API function --tested

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template, redirect, request, jsonify
+from flask import Flask, render_template, request, redirect, jsonify
+import requests
+
+
+API_KEY = "2e1083a5c426416f88a231919252912"
+
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
 
 
+    if request.method =="GET":
+        return render_template("welcome.html")
+    
+    
 
- 
+    if request.method == "POST":
+        city = request.form.get("location")
+        default = request.remote_addr or "New York City"
+
+
+        response = requests.get(f"http://api.weatherapi.com/v1/current.json",
+                     params={"key": API_KEY, "q": city or default}) 
+
+        data = response.json()
+        if data:
+            print(data)
+        return render_template("index.html", data=data)

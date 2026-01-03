00a061cf001fd5600102d1c26e657e6249e62b5f
Will N <146034825+HelpItsAllGooberedUp@users.noreply.github.com>
2026-01-02 21:52:05 -0600

    icon work/weather code number dictionary

---
+++ condition_icons.py
@@ -0,0 +1,21 @@
+CODE_TO_ICON = {
+
+    1000: "images/icons/wi-day-sunny.svg",
+    1003: "images/icons/wi-day-cloudy.svg",
+
+    1006: "images/icons/wi-cloud.svg",
+    1009: "images/icons/wi-cloud.svg",
+
+    1030: "images/icons/wi-fog.svg",
+    1063: "images/icons/wi-day-sprinkle-svg",
+    1069: "images/icons/wi-day-sleet.svg",
+    1072: "images/icons/wi-rain-mix.svg",
+    1087: "images/icons/wi-day-lightning.svg",
+    1114: "images/icons/wi-snow-wind/svg",
+    1117: "images/icons/wi-snowflake-cold.svg",
+    1135: "images/icons/wi-fog.svg",
+    1147: "images/icons/wi-snowflake-cold.svg",
+    1150: "images/icons/wi-sprinkle.svg"
+
+    
+}
